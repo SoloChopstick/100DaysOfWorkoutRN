34684d1822f8ca1b10802e29254d2a52521c5624
Han Wen Xie <hanw.xie@gmail.com>
2020-10-13 23:35:35 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,146 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  //Only works for IOS
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
+  //console.log(Dimensions.get("screen"));
+  //console.log(useDimensions());
+
+  const orientation = useDeviceOrientation();
+  //or
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <WelcomeScreen />
+    //The style on the RIGHT always overwrites the style on the LEFT
+    /*
+    <View
+      style={{
+        backgroundColor: "fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        //within each line in the cross axis
+        alignItems: "center",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        //flexWrap: "wrap",
+        //with wrapping enabled, align behaves differently, A LOT OF THINGS DONT WORK WITH WRAP!!!
+        //alignContent: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          height: 100,
+          width: 100,
+          alignSelf: "flex-start",
+          //equivalent to main AXIS weight/height
+          flexBasis: 100,
+          //fills available space, similar to flex positive, flexShrink similar to flex negative
+          //flexGrow: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          height: 100,
+          width: 100,
+          top: 20,
+          position: "absolute",
+        }}
+      />
+      <View style={{ backgroundColor: "tomato", height: 100, width: 100 }} />
+      <View style={{ backgroundColor: "grey", height: 100, width: 100 }} />
+      <View
+        style={{ backgroundColor: "greenyellow", height: 100, width: 100 }}
+      />
     </View>
+    */
+    /*
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          height: landscape ? "100%" : "30%",
+          width: "100%",
+        }}
+      ></View>
+      <Text>Hello React Native!</Text>
+
+      <Image source={require("./assets/icon.png")} />*
+      <Image
+        //blurRadius={10}
+        //fadeDuration={10000}
+        source={{
+          width: 200,
+          height: 300,
+          //reiszeMode: "cover",
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <Button
+        color="orange"
+        title="Click me"
+        onPress={() =>
+          Alert.alert("My Title", "My message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
+      {
+        //Only work on IOS right now
+      }
+      <Button
+        color="orange"
+        title="Click"
+        onPress={() =>
+          Alert.prompt("My title", "My message", (text) => console.log(text))
+        }
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+    */
   );
 }
 
+const containerStyle = { backgroundColor: "orange" };
+//Use this because this will validate the properties we put
+//optimization ?
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    //justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  View,
+  Button,
+  Image,
+  Text,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={{ uri: "https://picsum.photos/500/1000" }}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/favicon.png")}
+        ></Image>
+        <Text style={styles.title}> 100 DAYS OF WORKOUT!</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.signupButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  logo: {},
+  title: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 25,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  signupButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
