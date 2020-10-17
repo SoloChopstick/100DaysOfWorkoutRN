c038ef7c6c70e0329ae0007851e0905f74684d79
Han Wen Xie <hanw.xie@gmail.com>
2020-10-16 23:21:54 -0400

    Added ListItemDeleteAction Comonent

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, TouchableHighlight } from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText";
-function ListItem({ title, description, image }) {
+import Swipeable from "react-native-gesture-handler/Swipeable"
+function ListItem({ title, description, image, onPress, renderRightActions }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image}></Image>
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}> {title} </AppText>
-        <AppText style={styles.description}> {description} </AppText>
-      </View>
-    </View>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={image}></Image>
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}> {title} </AppText>
+            <AppText style={styles.description}> {description} </AppText>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
 import ListItemSeparator from "../components/ListItemSeparator";
-const messages = [
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+const initialMessages = [
   {
     id: 1,
     title: "T1",
@@ -18,6 +19,20 @@ const messages = [
   },
 ];
 function MessagesScreen(props) {
+  //ARRAY DESTRUCTURING BELOW
+  /*
+  const array = useState(0);
+  const count = array[0];
+  const setCount = array[1]; //setState
+  */
+
+  const [messages, setMessages] = useState(initialMessages);
+  const handleDelete = (message) => {
+    // Delete the message from messages
+    //const newMessages = messages.filter((msg) => msg.id !== message.id);
+    //setMessages(newMessages);
+    setMessages(messages.filter((msg) => msg.id !== message.id));
+  };
   return (
     <Screen>
       <FlatList
@@ -28,6 +43,10 @@ function MessagesScreen(props) {
             title={item.title}
             description={item.description}
             image={item.image}
+            onPress={() => console.log("YOLO", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
